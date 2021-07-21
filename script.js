b0d957867b6937b3a5c6c168afc0587c3b9f3148
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-07-21 05:03:45 +0300

    Add JS variables

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+"use strict";
 
+// +++++++++++ VARIABLES
+
+// Numbers
+const btnNum0 = document.querySelector(".btn-number-0");
+const btnNum1 = document.querySelector(".btn-number-1");
+const btnNum2 = document.querySelector(".btn-number-2");
+const btnNum3 = document.querySelector(".btn-number-3");
+const btnNum4 = document.querySelector(".btn-number-4");
+const btnNum5 = document.querySelector(".btn-number-5");
+const btnNum6 = document.querySelector(".btn-number-6");
+const btnNum7 = document.querySelector(".btn-number-7");
+const btnNum8 = document.querySelector(".btn-number-8");
+const btnNum9 = document.querySelector(".btn-number-9");
+
+const numsArr = [
+    btnNum0,
+    btnNum1,
+    btnNum2,
+    btnNum3,
+    btnNum4,
+    btnNum5,
+    btnNum6,
+    btnNum7,
+    btnNum8,
+    btnNum9,
+];
+
+// Operants
+const btnEqual = document.querySelector(".btn-equal");
+const btnPlus = document.querySelector(".btn-plus");
+const btnMinus = document.querySelector(".btn-minus");
+const btnMultiply = document.querySelector(".btn-multiply");
+const btnDivide = document.querySelector(".btn-divide");
+
+// Screens
+
+const screenCurrent = document.querySelector(".screen-current");
+const screenPrevious = document.querySelector(".screen-previous");
+
+// +++++++++++++ FUNCTIONS
+
+// +++++++++++++ EVENT LISTENERS
+console.log(numsArr);
